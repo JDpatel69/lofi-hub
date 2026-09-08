d1a9990a305a719a7294f58c5c499911d6903adc
Manan <manankalavadiya3@gmail.com>
2026-09-08 21:49:40 +0530

    task display done filter are remain

--- client/to-do.js
+++ client/to-do.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     var username = localStorage.getItem('username') || 'Guest';
     document.getElementById('username').textContent = username;
-
 });
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function () {
-   /* let input = document.getElementById("new-element");
-    let taskList = document.getElementById("task-list");
-    let username = document.getElementById('username').textContent = username; */
+    /* let input = document.getElementById("new-element");
+     let taskList = document.getElementById("task-list");
+     let username = document.getElementById('username').textContent = username; */
     if ((input.value.trim()) === "") {
         alert("Please enter a task!");
     }
     else {
-
-
-        const data ={
-            username:document.getElementById('username').textContent,
-            task:document.getElementById("new-element")
+        const data = {
+            username: document.getElementById('username').textContent,
+            task: document.getElementById("new-element")
         }
         fetch("http://localhost:3000/add-task", {
             method: "POST",
@@ -48,3 +45,41 @@ addBtn.addEventListener("click", function () {
             });
     }
 });
+
+function start() {
+    const data = {
+            username: document.getElementById('username').textContent,
+        }
+        fetch("http://localhost:3000/get-tasks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json()
+                        .then(errData => {
+                            throw new Error(errData.message || "Error try Again");
+                        });
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.success) {
+                    let taskList = document.getElementById("task-list");
+                    data.tasks.forEach(task => {
+                        let newTask = document.createElement("li");
+                        newTask.textContent = task;
+                        taskList.appendChild(newTask);
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert(error.message || "Failed to add country!");
+            });
+
+
+}
